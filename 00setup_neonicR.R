a39f2110577b565087b936ced6f1bfb537b626fc
Carmen Kuan <ashleyc.kuan@gmail.com>
2015-09-21 16:15:21 -0400

    separate neonicR, fix plotting labels

---
+++ 00setup_neonicR.R
@@ -0,0 +1,132 @@
+#Determine path directory ######
+
+#windows
+if(Sys.info()[4]=="DC2626UTPURUCKE"){
+  vpdir_input<-path.expand("C:\\git\\Dropbox\\NeonicR\\input\\")
+  vpdir_output<-path.expand("C:\\git\\Dropbox\\NeonicR\\output\\")
+  #vpdir_exe<-path.expand("C:\\Program Files (x86)\\VarroaPop\\")
+  vpdir_exe<-path.expand("C:\\git\\Dropbox\\NeonicR\\exe\\")
+}
+
+#marcia epa computer
+if(Sys.info()[4]=="LZ2626UMSNYDE02"){
+  vpdir<-path.expand("C:\\Users\\msnyde02\\varroapoptest2\\")
+}
+
+#system("C:\\Users\\msnyde02\\varroapoptest2\\VarroaPop.exe C:\\Users\\msnyde02\\varroapoptest2\\Default.vrp /b /or C:\\Users\\msnyde02\\varroapoptest2\\results7.txt /i C:\\Users\\msnyde02\\varroapoptest2\\queenvar.txt")
+
+#carmen personal laptop
+
+if(Sys.info()[4]=="Ashleys-MBP"){
+  vpdir<-path.expand("~/git/beeRpop/")
+  vpdir_input<-path.expand("~/git/beeRpop/input/")
+  vpdir_output<-path.expand("~/git/beeRpop/output/")
+  vpdir_exe<-path.expand("~/git/beeRpop/exe/")
+}
+if(Sys.info()[4]=="ACKUAN-PC"){
+  vpdir<-path.expand("C:\\gitrepo\\beeRpop\\")
+  vpdir_input<-path.expand("C:\\gitrepo\\beeRpop\\input\\")
+  vpdir_output<-path.expand("C:\\gitrepo\\beeRpop\\output\\")
+  vpdir_exe<-path.expand("C:\\gitrepo\\beeRpop\\exe\\")
+}
+
+#library packages
+library(plyr)
+library(reshape2)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(sensitivity)
+library(abind)
+
+#create input files #########
+
+queenstrength<- runif(1000, 1, 6) #ICQueenStrength
+wkrdrnratio<- runif(1000, 1, 5) #RQWkrDrnRatio
+drnmitesurvive<- runif(1000, 0, 100) #ICDroneMiteSurvivorship
+wkrmitesurvive<- runif(1000, 0, 100) #ICWorkerMiteSurvivorship
+fgrlifespan <- runif(1000, 4, 16) #ICForagerLifespan
+immtype <- c("Polynomial", "Cosine", "Sine", "Exponential", "Tangent", "Logarithmic")
+miteimmtype <- sample(immtype, size=1000, replace=T) #ImmType
+
+#pesticide exposure
+adslope<- runif(1000, 0, 3.4) #AIAdultSlope
+adLD50<- runif(1000, 0, 3) #AIAdultLD50 (ug/bee)
+adslopec<- runif(1000, 0, 3.93) #AIAdultSlopeContact
+adLD50c<- runif(1000, 0, 0.05) #AIAdultLD50Contact (ug/bee)
+lslope<- runif(1000, 0, 3.4) #AILarvaSlope
+lLD50<- runif(1000, 0, 0.9) #AILarvaLD50 (ug/larva)
+kow<- runif(1000, 2, 8) #AIKOW
+koc<- runif(1000, 5, 30) #AIKOC
+halflife<- runif(1000, 0, 14) #AIHalfLife (days)
+apprate<- runif(1000, 0, 1) #EAppRate (lb/A)
+
+
+for (i in 1:1000) {
+  
+  parameter <- ("ICQueenStrength=") #queen strength
+  inputvalue <- queenstrength[i]
+  parameter2 <- ("RQWkrDrnRatio=") #requeen worker-drone ratio
+  inputvalue2 <- wkrdrnratio[i]
+  parameter3 <- ("ICDroneMiteSurvivorship=") #drone-mite survivorship
+  inputvalue3 <- drnmitesurvive[i]
+  parameter4 <- ("ICWorkerMiteSurvivorship=")  #worker-mite survivorship
+  inputvalue4 <- wkrmitesurvive[i]
+  parameter5 <- ("ICForagerLifespan=")  #Forager Lifespan
+  inputvalue5 <- fgrlifespan[i]
+  parameter6 <- ("ImmType=")   #Mite Immigration Type
+  inputvalue6 <- miteimmtype[i]
+  parameter7 <- ("AIAdultSlope=") #ai adult slope
+  inputvalue7 <- adslope[i]
+  parameter8 <- ("AIAdultLD50=") #ai adult LD50
+  inputvalue8 <- adLD50[i]
+  parameter9 <- ("AIAdultSlopeContact=") #ai adult slope contact
+  inputvalue9 <- adslopec[i]
+  parameter10 <- ("AIAdultLD50Contact=") #ai adult LD50 contact
+  inputvalue10 <- adLD50c[i]
+  parameter11 <- ("AILarvaSlope=") #ai larva slope
+  inputvalue11 <- lslope[i]
+  parameter12 <- ("AILarvaLD50=") #ai larva LD50
+  inputvalue12 <- lLD50[i]
+  parameter13 <- ("AIKOW=") #ai kow
+  inputvalue13 <- kow[i]
+  parameter14 <- ("AIKOC=") #ai koc
+  inputvalue14 <- koc[i]
+  parameter15 <- ("AIHalfLife=") #ai half life (days)
+  inputvalue15 <- halflife[i]
+  parameter16 <- ("EAppRate=") #application rate (lb/A)
+  inputvalue16<- apprate[i]
+  
+  varroainput <- paste(parameter,inputvalue, sep=" ")
+  write(varroainput, file = paste(vpdir_input, "input",i,".txt", sep = ""), append = FALSE)
+  varroainput2 <- paste(parameter2,inputvalue2, sep=" ")
+  write(varroainput2, file = paste(vpdir_input, "input",i,".txt", sep = ""), append = TRUE)
+  varroainput3 <- paste(parameter3, inputvalue3, sep=" ")
+  write(varroainput3, file = paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput4 <- paste(parameter4, inputvalue4, sep=" ")
+  write(varroainput4, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput5 <- paste(parameter5, inputvalue5, sep= " ")
+  write(varroainput5, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput6 <- paste(parameter6, inputvalue6, sep= " ")
+  write(varroainput6, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput7 <- paste(parameter7, inputvalue7, sep= " ")
+  write(varroainput7, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput8 <- paste(parameter8, inputvalue8, sep= " ")
+  write(varroainput8, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput9 <- paste(parameter9, inputvalue9, sep= " ")
+  write(varroainput9, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput10 <- paste(parameter10, inputvalue10, sep= " ")
+  write(varroainput10, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput11 <- paste(parameter11, inputvalue11, sep= " ")
+  write(varroainput11, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput12 <- paste(parameter12, inputvalue12, sep= " ")
+  write(varroainput12, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput13 <- paste(parameter13, inputvalue13, sep= " ")
+  write(varroainput13, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput14 <- paste(parameter14, inputvalue14, sep= " ")
+  write(varroainput14, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput15 <- paste(parameter15, inputvalue15, sep= " ")
+  write(varroainput15, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+  varroainput16 <- paste(parameter16, inputvalue16, sep= " ")
+  write(varroainput16, file= paste(vpdir_input, "input", i, ".txt", sep=""), append= TRUE)
+}
---
+++ 01simulation_neonicR.R
@@ -0,0 +1,37 @@
+#run simulations###########
+#command to run simulations in varroapop with relative directory structure and write output files
+
+# inputtest<- c(8, 54, 62, 65, 69, 70, 73, 94, 134, 137, 155, 164, 179, 195, 197, 198, 
+#              203, 214, 216, 233, 246, 260, 263, 274, 277, 288, 292, 314, 324, 328, 340,
+#              344, 369, 372, 383, 388, 391, 400, 409, 419, 437, 452, 460, 463, 464, 480,
+#              484, 491, 505, 522, 557, 579, 586, 600, 604, 605, 613, 618, 624, 638, 646,
+#              657, 679, 702, 713, 727, 737, 743, 749, 750, 768, 771, 777, 782, 786, 796,
+#              798, 799, 805, 807, 848, 855, 857, 871, 875, 877, 888, 889, 909, 928, 943,
+#              949, 954, 962, 996)
+
+for (i in 1:1000) {
+  # n<-inputtest[i]
+  inputfile<- paste("input",i,".txt", sep="")
+  outputfile<- paste("results",i,".txt", sep="")
+  logfile<- paste("log",i,".txt", sep="")
+  vpdir_command <- paste(vpdir_exe, "VarroaPop.exe ", vpdir_exe, "Default.vrp", 
+                         " /b /or ", vpdir_output, outputfile, 
+                         " /i ", vpdir_input, inputfile, " /ol ", vpdir_output, logfile, sep="")
+  print(vpdir_command)
+  system(vpdir_command)
+}
+
+# 3d array ######
+
+tdarray <- array(data=NA,c(1827,26,1000))
+dim(tdarray)
+# read output files
+for (i in 1:1000) {
+  df<- read.table(paste(vpdir_output,"results",i,".txt", sep=""), header= FALSE, sep= "", 
+                  skip = 6, stringsAsFactors = FALSE, row.names=NULL)
+  newarray <- df[,2:27]
+  newarray2<- data.matrix(newarray)
+  tdarray[1:1827,1:26,i] <- abind(newarray2[1:1827,1:26], along=3)
+}
+# save tdarray
+save(tdarray, file = paste(vpdir_output,"tdarray.RData", sep = ""))
