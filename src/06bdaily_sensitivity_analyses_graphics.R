f32c14c72e239253099d8a87a5a7d23b1b9a7912
Tom Purucker <purucker.tom@gmail.com>
2016-09-05 19:06:28 +0000

    daily plot for publication plus legend

--- src/06bdaily_sensitivity_analyses_graphics.R
+++ src/06bdaily_sensitivity_analyses_graphics.R
@@ -1,4 +1,51 @@
 ########### control daily sensitivity plot
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+
 #load control pcc results
 load(paste(vpdir_out_control,"tdarray_pccout_control.RData", sep = ""))
 dim(tdarray_pccout_control)
@@ -7,15 +54,16 @@ dim(tdarray_pccout_control)
 colnames(tdarray_pccout_control) <- colnames(inputdata_control)
 colnames(tdarray_pccout_control)
 date <- 61:ndays
-date2 <- seq(as.Date("1910/1/1"), as.Date("1999/1/1"), "years")
+date2 <- seq(as.Date("1988/1/1"), as.Date("1991/1/1"), "days")
+date3 <- date2[61:ndays]
 
 #qs = melted_control$value[which(melted_control$variable=="queenstrength")]
 #plot control daily sensitivities
 pcc_control <- as.data.frame(cbind(date, tdarray_pccout_control[61:ndays,]))
 colnames(pcc_control)
 melted_control = melt(pcc_control, id.vars="date")
-pdf(file= paste(vpdir_fig, "daily_sensitivity_control.pdf", sep=""), width = 8, height = 6)
-  ggplot(melted_control, aes(x=date, y=value, group=variable)) +
+
+daily_sensitivity_control <-  ggplot(melted_control, aes(x=date, y=value, group=variable)) +
     geom_line(aes(colour=melted_control$variable)) +
     scale_colour_manual(values = c(
       "firebrick3",  "steelblue", "steelblue",   "steelblue",    "blue",     "firebrick",  "steelblue", "steelblue",
@@ -35,9 +83,14 @@ pdf(file= paste(vpdir_fig, "daily_sensitivity_control.pdf", sep=""), width = 8, 
     guides(fill=FALSE) +  
     xlab("Simulation Day") + 
     ylab("Partial Correlation Coefficient") +
-    ggtitle("Daily Sensitivity (PCC) for Control Scenario") +
+    #ggtitle("Daily Sensitivity (PCC) for Control Scenario") +
+    annotate("text", x = 1000, y = 0.92, label = "Control", size=6) +
     theme_bw() +
-    theme(legend.position = "none")
+    scale_x_discrete(breaks = c(61,426,605,791,1035)) +
+    theme(legend.position = "none",  axis.title.x=element_blank(), axis.text.x=element_blank())
+
+pdf(file= paste(vpdir_fig, "daily_sensitivity_control.pdf", sep=""), width = 8, height = 6)
+  daily_sensitivity_control
 dev.off()
 
 ########### foliar daily sensitivity plot
@@ -54,8 +107,8 @@ date <- 61:ndays
 pcc_foliar <- as.data.frame(cbind(date, tdarray_pccout_foliar[61:ndays,]))
 colnames(pcc_foliar)
 melted_foliar = melt(pcc_foliar, id.vars="date")
-pdf(file= paste(vpdir_fig, "daily_sensitivity_foliar.pdf", sep=""), width = 8, height = 6)
-  ggplot(melted_foliar, aes(x=date, y=value, group=variable)) +
+
+daily_sensitivity_foliar <- ggplot(melted_foliar, aes(x=date, y=value, group=variable)) +
     geom_line(aes(colour=melted_foliar$variable)) +
     scale_colour_manual(values = c(
       "firebrick3","steelblue", "steelblue","steelblue",       "blue",    "firebrick",  "steelblue","steelblue", 
@@ -80,9 +133,14 @@ pdf(file= paste(vpdir_fig, "daily_sensitivity_foliar.pdf", sep=""), width = 8, h
         guides(fill=FALSE) +  
     xlab("Simulation Day") + 
     ylab("Partial Correlation Coefficient") +
-    ggtitle("Daily Sensitivity (PCC) for Foliar Scenario") +
+    #ggtitle("Daily Sensitivity (PCC) for Foliar Scenario") +
+    annotate("text", x = 1000, y = 0.92, label = "Foliar", size=6) +
     theme_bw() +
-    theme(legend.position = "none")
+    scale_x_discrete(breaks = c(61,426,605,791,1035)) +
+    theme(legend.position = "none",  axis.title.x=element_blank(), axis.text.x=element_blank())
+
+pdf(file= paste(vpdir_fig, "daily_sensitivity_foliar.pdf", sep=""), width = 8, height = 6)
+  daily_sensitivity_foliar
 dev.off()
 
 ########### soil daily sensitivity plot
@@ -99,8 +157,8 @@ date <- 61:ndays
 pcc_soil <- as.data.frame(cbind(date, tdarray_pccout_soil[61:ndays,]))
 colnames(pcc_soil)
 melted_soil = melt(pcc_soil, id.vars="date")
-pdf(file= paste(vpdir_fig, "daily_sensitivity_soil.pdf", sep=""), width = 8, height = 6)
-  ggplot(melted_soil, aes(x=date, y=value, group=variable)) +
+
+daily_sensitivity_soil <- ggplot(melted_soil, aes(x=date, y=value, group=variable)) +
     geom_line(aes(colour=melted_soil$variable)) +
     scale_colour_manual(values = c(
       "firebrick3","steelblue", "steelblue",  "steelblue",  "blue",     "firebrick", "steelblue", "steelblue", 
@@ -125,9 +183,14 @@ pdf(file= paste(vpdir_fig, "daily_sensitivity_soil.pdf", sep=""), width = 8, hei
     guides(fill=FALSE) +  
     xlab("Simulation Day") + 
     ylab("Partial Correlation Coefficient") +
-    ggtitle("Daily Sensitivity (PCC) for Soil Scenario") +
+    #ggtitle("Daily Sensitivity (PCC) for Soil Scenario") +
+    annotate("text", x = 1000, y = 0.92, label = "Soil", size=6) +
     theme_bw() +
-    theme(legend.position = "none")
+    scale_x_discrete(breaks = c(61,426,605,791,1035)) +
+    theme(legend.position = "none",  axis.title.x=element_blank(), axis.text.x=element_blank())
+  
+pdf(file= paste(vpdir_fig, "daily_sensitivity_soil.pdf", sep=""), width = 8, height = 6)
+  daily_sensitivity_soil
 dev.off()
 
 
@@ -145,8 +208,8 @@ date <- 61:ndays
 pcc_seed <- as.data.frame(cbind(date, tdarray_pccout_seed[61:ndays,]))
 colnames(pcc_seed)
 melted_seed = melt(pcc_seed, id.vars="date")
-pdf(file= paste(vpdir_fig, "daily_sensitivity_seed.pdf", sep=""), width = 8, height = 6)
-ggplot(melted_seed, aes(x=date, y=value, group=variable)) +
+
+daily_sensitivity_seed <- ggplot(melted_seed, aes(x=date, y=value, group=variable)) +
   geom_line(aes(colour=melted_seed$variable)) +
   scale_colour_manual(values = c(
     "firebrick3","steelblue", "steelblue",  "steelblue",  "blue",     "firebrick", "steelblue", "steelblue", 
@@ -171,8 +234,22 @@ ggplot(melted_seed, aes(x=date, y=value, group=variable)) +
   guides(fill=FALSE) +  
   xlab("Simulation Day") + 
   ylab("Partial Correlation Coefficient") +
-  ggtitle("Daily Sensitivity (PCC) for Seed Scenario") +
+  #ggtitle("Daily Sensitivity (PCC) for Seed Scenario") +
+  annotate("text", x = 1000, y = 0.92, label = "Seed", size=6) +
   theme_bw() +
+  #august 9 is day 221, +366 (leap year) = 587
+  scale_x_discrete(breaks = c(61,426,605,791,1035), labels = c("3/1/1988","3/1/1989","8/27/1989","3/1/1990","10/31/1990")) +
   theme(legend.position = "none")
+
+pdf(file= paste(vpdir_fig, "daily_sensitivity_seed.pdf", sep=""), width = 8, height = 6)
+  daily_sensitivity_seed
 dev.off()
 
+######plot all 4 together
+pdf(file= paste(vpdir_fig, "daily_sensitivity_combined.pdf", sep=""), width = 7, height = 10)
+  multiplot(daily_sensitivity_control, daily_sensitivity_foliar, daily_sensitivity_soil, daily_sensitivity_seed, cols=1)
+dev.off()
+
+png(file= paste(vpdir_fig, "daily_sensitivity_combined.png", sep=""), width = 7, height = 10, units='in', pointsize=12)
+  multiplot(daily_sensitivity_control, daily_sensitivity_foliar, daily_sensitivity_soil, daily_sensitivity_seed, cols=1)
+dev.off()
