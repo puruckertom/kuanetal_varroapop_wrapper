5ac701c3393805c174a42d3b41c3fcd4264ff3be
Tom Purucker <purucker.tom@gmail.com>
2016-09-29 20:27:23 +0000

    adding tscf calculations 2

--- src/14compare_tscfs.R
+++ src/14compare_tscfs.R
@@ -7,17 +7,25 @@ tscf2012 <- 0.784*10^(-4.34*(logKow-1.78)^2/2.44)
 #2014 beerex guidance
 tscf2014 <- -0.0648*(logKow)^2 + 0.241*logKow + 0.5822
 
+#0.2431 instead of 0.241 in beerex for tscf2014??
+
 plot(logKow,tscf2014,type='l',col='red')
 lines(logKow,tscf2012,type='l')
 
 csoil <- 1.0
-rho <- 2.5
-theta <- 1-(rho/2.65)
-koc <- 10000
+rho <- 1.5
+#theta <- 1-(rho/2.65) ??
+theta <- 0.2
+koc <- 30
 foc <- 0.01
 #this equation is the same in both the 2012 white paper and the 2014 beerex manual
-c_stem_2012 <- (10^(.95*logKow-2.05)+0.82)*tscf2012*(rho/(theta+rho*koc*foc))*csoil
-c_stem_2014 <- (10^(.95*logKow-2.05)+0.82)*tscf2014*(rho/(theta+rho*koc*foc))*csoil
+conc_stem2012 <- (10^(.95*logKow-2.05)+0.82)*tscf2012*(rho/(theta+rho*koc*foc))*csoil
+conc_stem2014 <- (10^(.95*logKow-2.05)+0.82)*tscf2014*(rho/(theta+rho*koc*foc))*csoil
 
-plot(logKow,c_stem_2014,type='l',col='red')
-lines(logKow,c_stem_2012,type='l')
+plot(logKow,conc_stem2014,type='l',col='red')
+lines(logKow,conc_stem2012,type='l')
+
+
+conc_ratio <- conc_stem2014/conc_stem2012
+#View(cbind(conc_stem2014,conc_stem2012,conc_ratio))
+plot(logKow,conc_ratio)
